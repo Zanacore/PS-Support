fa7a5838932432cd5e602013a66796350ae512d9
Jason Poole <jason@zanacore.com>
2018-03-14 14:49:54 -0400

    Added Base Printer Logic Install Script

---
+++ Printer_Logic_Install.ps1
@@ -0,0 +1,17 @@
+﻿#Custom for each Client
+$HomeURL = "https://zanacore.printercloud.com"
+$AuthCode = "flnvx3"
+
+#You should not need to edit below this line
+$ClientName = "PrinterInstallerClient.msi"
+$URLPath = $HomeURL + "/client/setup/$ClientName"
+$TempPath = "C:\TempPath\"
+
+#Test Temp Path before trying to download
+If(!(Test-Path -path $TempPath)) {New-Item -path $TempPath -Itemtype Directory}
+
+#Download the Client software
+Invoke-WebRequest -Uri $URLPath -OutFile "C:\TempPath\PrinterInstallerClient.msi"
+
+#Run the installer
+& $TempPath$ClientName /qn HomeURL=$HomeURL Authorization_Code=$AuthCode
