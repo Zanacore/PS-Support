2e38bbc165b3f20cd756dc1fc120941eeed9c714
Jason Poole <jason@zanacore.com>
2018-03-19 16:41:04 -0400

    Add Map Drive Script Sample

---
+++ Map_Drives.ps1
@@ -0,0 +1,3 @@
+﻿#This will map the drive and prompt for Password. If you remove the credential then it will use the currently login user
+#New-PSDrive -Name "S" -Root "\\Server01\Scripts" -Persist -PSProvider "FileSystem" -Credential "Domain\User"
+New-PSDrive -Name "S" -Root "\\192.168.1.10\Zanacore" -Persist -PSProvider "FileSystem" -Credential "Zanacore\Jason"
